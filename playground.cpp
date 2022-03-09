e2b1a84cf9ec6c7e7fc7540a6988c8bda8f18184
Andrew Orazov <orazov.ae@phystech.edu>
2022-03-09 09:49:38 +0300

    Apply basic texture

--- playground.cpp
+++ playground.cpp
@@ -10,21 +10,29 @@ GLFWwindow* window;
 #include <glm/glm.hpp>
 using namespace glm;
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 float data_buf[] = {
-  0.5f, 0.5f, .2f, 
+  0.5f, 0.5f, .2f,
   1.f, 0.f, 0.f,  0.f,
+  0.f, 0.f,
 
   0.5f, -0.5f, .3f,
   1.f, 1.f, 0.f,  0.f,
+  1.f, 1.f,
 
   0.f, 0.f, 0.9f,
   0.f, 0.f, 1.f,  2.f,
+  1.f, 0.f,
 
   -0.5f, 0.5f, 0.f,
   1.f, 0.f, 0.f,  0.f,
+  1.f, 1.f,
 
   -0.5f, -0.5f, 0.f,
   1.f, 0.f, 0.f,  0.f,
+  1.f, 1.f,
 };
 
 uint32_t index_array[] = {
@@ -34,39 +42,55 @@ uint32_t index_array[] = {
 
 char const* vertex_shader =
 "#version 330 core\n"
+
 "layout (location=0) in vec3 position;\n"
 "layout (location=1) in vec3 color;\n"
 "layout (location=2) in float flag;\n"
+"layout (location=3) in vec2 tex_coord;\n"
+
 "out vec3 in_color;\n"
+"out vec2 in_tex_coord;\n"
 "out float in_flag;\n"
+
 "uniform float offset;\n"
+
 "void main()\n"
 "{\n"
 "  gl_Position = vec4(offset / 2 + position.x, position.yz, 1.0);\n"
 "  in_color = vec3(offset / 2 + position.x / 2 + 0.5, position.y / 2 + 0.5, position.z);\n"
 "  in_flag = flag;\n"
+"  in_tex_coord = tex_coord;\n"
+"}\n";
+
+char const* fragment_shader2 =
+"#version 330 core\n"
+
+"out vec4 color;\n"
+"in vec2 in_tex_coord;\n"
+
+"uniform sampler2D sampler;\n"
+
+"void main()\n"
+"{\n"
+"  color = texture(sampler, in_tex_coord);\n"
 "}\n";
 
 char const* fragment_shader =
 "#version 330 core\n"
+
 "out vec3 color;\n"
+
 "in vec3 in_color;\n"
 "in float in_flag;\n"
+
 "uniform vec3 our_color;\n"
+
 "void main()\n"
 "{\n"
 "  if (in_flag > 1.0)\n"
 "    color = our_color;\n"
 "  else \n"
 "    color = in_color;\n"
-"}\n";
-
-char const* fragment_shader2 =
-"#version 330 core\n"
-"out vec3 color;\n"
-"void main()\n"
-"{\n"
-"  color = vec3(1.0, 0.0, 0.0);\n"
 "}\n";
 
 int main( void )
@@ -93,6 +117,41 @@ int main( void )
 
   // Dark blue background
   glClearColor(0.f, 0.f, 0.4f, 0.f);
+
+
+
+
+
+
+
+
+  int width, height, nrChannels;
+  unsigned char *data = stbi_load("container.jpg", &width, &height,
+      &nrChannels, 0);
+
+
+
+
+
+
+  unsigned int texture = 0;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+      GL_UNSIGNED_BYTE, data);
+  glGenerateMipmap(GL_TEXTURE_2D);
+
+
+
+
+
+
+  stbi_image_free(data);
+
+
+
 
 
 
@@ -125,7 +184,8 @@ int main( void )
   uint32_t ebo = 0;
   glGenBuffers(1, &ebo);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index_array), index_array, GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index_array), index_array,
+      GL_STATIC_DRAW);
 
 
 
@@ -133,15 +193,17 @@ int main( void )
 
 
   // Set vao to point into buffer
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 7, NULL);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 9, NULL);
   glEnableVertexAttribArray(0);
 
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (void*)(12));
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 9, (void*)(12));
   glEnableVertexAttribArray(1);
 
-  glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (void*)(24));
+  glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, sizeof(float) * 9, (void*)(24));
   glEnableVertexAttribArray(2);
 
+  glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 9, (void*)(28));
+  glEnableVertexAttribArray(3);
 
 
 
@@ -177,9 +239,18 @@ int main( void )
 
 
 
+
+
+
+
+
   int vertexColorLocation = glGetUniformLocation(prog_id, "our_color");
   int offset_loc = glGetUniformLocation(prog_id, "offset");
-  int offset_loc2 = glGetUniformLocation(prog_id2, "offset");
+
+
+
+
+
 
 
 
@@ -188,6 +259,9 @@ int main( void )
 
     glUseProgram(prog_id);
 
+
+
+
     // update shader uniform
     double timeValue = glfwGetTime();
     float greenValue = static_cast<float>(sin(timeValue) / 4.0 + 0.5);
@@ -195,13 +269,28 @@ int main( void )
     glUniform1f(offset_loc, greenValue);
 
 
+
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+
+
+
+    glDrawElementsBaseVertex(GL_TRIANGLES, 3, GL_UNSIGNED_INT,
+        NULL, 0);
+
+
+
+
+
+
+
+    glUseProgram(prog_id2);
+
+    glBindTexture(GL_TEXTURE_2D, texture);
+
     glDrawElementsBaseVertex(GL_TRIANGLES, 3, GL_UNSIGNED_INT,
         NULL, 2);
 
-    glUseProgram(prog_id2);
-    glUniform1f(offset_loc2, 2 - greenValue * 2);
-    glDrawElementsBaseVertex(GL_TRIANGLES, 3, GL_UNSIGNED_INT,
-        NULL, 0);
 
 
     // Swap buffers
